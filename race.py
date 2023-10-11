6b1832dbc0eba5885137a11903ab088d9412c096
iamrogine <147251397+iamrogine@users.noreply.github.com>
2023-10-11 15:21:02 +0800

    Removed unnecessary codes
    Rogine Alamo

--- race.py
+++ race.py
@@ -40,31 +40,4 @@ while is_race_on:
 
 
 
-
-
-
-#ETCH-A-SKETCH
-# def move_forward():
-#     tim.fd(10)
-#
-# def move_backward():
-#     tim.backward(10)
-#
-# def clockwise():
-#     t = tim.heading() + 10
-#     tim.setheading(t)
-#
-# def counter_clockwise():
-#     t = tim.heading() - 10
-#     tim.setheading(t)
-#
-# def clear():
-#     tim.reset()
-#
-# screen.listen()
-# screen.onkey(key="w", fun=move_forward)
-# screen.onkey(key="s", fun=move_backward)
-# screen.onkey(key="a", fun=clockwise)
-# screen.onkey(key="d", fun=counter_clockwise)
-# screen.onkey(key="c", fun=clear)
 screen.exitonclick()
